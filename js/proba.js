8068d9a47ab25a5d4f5417268a30a87090956d77
Cabt1995 <aprende_facil@outlook.es>
2018-04-26 20:14:33 -0500

    Add files via upload

---
+++ js/proba.js
@@ -0,0 +1,37 @@
+var player;
+function onYouTubeIframeAPIReady() {
+        player = new YT.Player('video-placeholder', {
+        videoId: 'fSvNycU6lBE',
+        playerVars: {
+        color: 'white',
+        playlist: 'hjvtv3KLmJE',
+        showinfo: '1',
+        rel:'0',
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
+             
+function initialize(){
+    // Update the controls on load
+        updateTimerDisplay();
+        updateProgressBar();
+    // Clear any old interval.
+        clearInterval(time_update_interval);
+    // Start interval to update elapsed time display and
+    // the elapsed part of the progress bar every second.
+        time_update_interval = setInterval(function () {
+        updateTimerDisplay();
+        updateProgressBar();
+    }, 1000)
+}
+
+$('#next').on('click', function () {
+    player.nextVideo()
+});
+
+$('#prev').on('click', function () {
+    player.previousVideo()
+});
